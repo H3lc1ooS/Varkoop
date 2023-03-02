0b1b74d65b8f29be7bc522871106ba02cbfd58a7
H3lc1ooS <helciosilva@protonmail.com>
2023-03-03 00:52:59 +0100

    feat: :tada: Criei e configurei a migration relacionada ao produto

---
+++ database/migrations/2023_03_02_225624_create_produtos_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('user_id')
+            //     ->constrained('users')
+            //     ->cascadeOnDelete()
+            //     ->cascadeOnUpdate();
+
+            $table->string('nome_produto', 150);
+            $table->longText('descricao_produto');
+            // $table->foreignId('estado_produto_id')
+            //     ->nullable()
+            //     ->constrained('estados')
+            //     ->cascadeOnDelete()
+            //     ->cascadeOnUpdate();
+
+            $table->string('preco_produto', 15);
+            // $table->foreignId('cor_produto_id')
+            //     ->constrained('cores')
+            //     ->cascadeOnDelete()
+            //     ->cascadeOnUpdate();
+
+            // $table->foreignId('memoria_produto_id')
+            //     ->nullable()
+            //     ->constrained('memorias')
+            //     ->cascadeOnDelete()
+            //     ->cascadeOnUpdate();
+
+            $table->integer('quantidade_produto');
+            // $table->foreignId('categoria_produto_id')
+            //     ->constrained('categorias')
+            //     ->cascadeOnDelete()
+            //     ->cascadeOnUpdate();
+
+            $table->integer('vendidos_produto');
+            $table->string('imagem_produto');
+            $table->string('frete_produto', 15);
+            $table->string('data_entrega_produto', 4);
+            $table->longText('nota_produto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+<h1>Tela principal</h1>
+
+<a href="/login">Login</a>
+<a href="/register">Register</a>
